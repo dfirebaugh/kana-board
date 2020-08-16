c6a6c04fba331d3d972b8ad0b36b00858c9b81a7
Dustin Firebaugh <dafirebaugh@gmail.com>
2020-08-15 23:26:02 -0400

    fixing issues with image compile script not adding mnemonic for images that don't exist now adding the proper image instead of the same 'a' image

--- data/toBase64.js
+++ data/toBase64.js
@@ -7,29 +7,30 @@ import fs from "fs";
 
 // function to encode file data to base64 encoded string
 function base64_encode(file) {
-    // read binary data
+    if (!fs.existsSync(file)) return;
+
     var bitmap = fs.readFileSync(file);
+
     // convert binary data to base64 encoded string
     return new Buffer(bitmap).toString('base64');
 }
 
 const kanasObj = {
-
 }
 
 /**
  * attach base64 of image to each object
  */
-Object.keys(kanas).forEach( key => {
+Object.keys(kanas).forEach(key => {
     kanasObj[key] = Object.assign({}, kanas[key], {
-        mnemonic: base64_encode(`./data/images/mnemonics/${kanas.a.roumaji}.jpg`)
+        mnemonic: base64_encode(`./data/images/mnemonics/${kanas[key].roumaji}.jpg`)
     });
 })
 
 const OutputString = `export default ${JSON.stringify(kanasObj)}`;
 
-    // console.log(OutputString)
-fs.writeFile(`./data/kanas_with_images.js`, OutputString, function(err) {
+// console.log(OutputString)
+fs.writeFile(`./data/kanas_with_images.js`, OutputString, function (err) {
     if (err) {
         console.error(err);
     }
